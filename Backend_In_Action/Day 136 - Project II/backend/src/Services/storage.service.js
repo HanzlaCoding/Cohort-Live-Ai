533e4de1dce6f141f96612e21fefe13a48e92253
= <=>
2025-09-25 22:34:09 +0500

    Only Backend - Project-2

--- Backend_In_Action/Day 136 - Project II/backend/src/Services/storage.service.js
+++ Backend_In_Action/Day 136 - Project II/backend/src/Services/storage.service.js
@@ -1,12 +1,6 @@
-// Load environment variables from .env file
 require("dotenv").config();
-
-// Require the ImageKit library and the Node.js file system module
 const ImageKit = require("imagekit");
-const fs = require("fs");
 
-// 1. Initialize ImageKit Client
-// Use your credentials from the .env file
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
