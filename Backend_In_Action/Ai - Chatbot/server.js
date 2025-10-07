925f92953879252fb5d43dcd69312a8a25018044
= <=>
2025-10-07 23:16:28 +0500

    feat: Set up server with Socket.io and integrate Google GenAI for message generation

--- Backend_In_Action/Ai - Chatbot/server.js
+++ Backend_In_Action/Ai - Chatbot/server.js
@@ -1,12 +1,30 @@
 const app = require("./src/app");
+require("dotenv").config();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const generateMessage = require("./src/services/ai.service");
 
-const httpServer = createServer(app);
+const httpServer = createServer();
 const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("a user connected!");
+
+  socket.on("disconnect", () => {
+    console.log("user is disconnected!");
+  });
+
+  socket.on("message", async (message) => {
+
+    const response = await generateMessage(message);
+    socket.emit("response", response);
+    console.log(response);
+    
+  });
 });
 
 httpServer.listen(3000);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
